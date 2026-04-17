85f63af3428717675ab670e27bbacb77e40ae79a
thermite <ignite.hsn@gmail.com>
2026-04-17 16:39:34 -0400

    Build system fix

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11.setup_helpers import Pybind11Extension, build_ext as build_ext_orig
+
+class build_ext(build_ext_orig):
+    def build_extensions(self):
+        original_compile = self.compiler._compile
+
+        def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+            if src.endswith(".c"):
+                extra_postargs = [
+                    arg for arg in (extra_postargs or [])
+                    if not arg.startswith("-std=c++")
+                ]
+            return original_compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+
+        self.compiler._compile = _compile
+        super().build_extensions()
 
 setup(
     ext_modules=[
